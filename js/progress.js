82ccb190508607b541309adb633f996fcc3387df
Alex <alextec70@outlook.com>
2024-09-23 19:23:50 +0300

    sharedweb streamline 1.0
    Move common ATP & VI assets to sharedweb git submodule

--- js/progress.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    var progressBars = document.querySelectorAll(".progress-bar");
-    
-    progressBars.forEach(function(progressBar) {
-      var desiredPercentage = progressBar.dataset.progress;
-      progressBar.style.width = desiredPercentage + "%";
-    });
-  });
-  
